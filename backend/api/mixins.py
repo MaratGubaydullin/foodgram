388b6d4e8514910395fa60c2a72e2de2f193fc00
mgubaydullin <mgubaydullin@131.ru>
2025-02-22 15:48:13 +0300

    main 10 flake8 and pep8 fix
    flake8 and pep8 fix

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -2,8 +2,8 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
-from users.models import Follow, Recipe, User
 
+from users.models import Follow, Recipe, User
 from api.serializers import SubscriptionSerializer
 
 
