e1848e5045cdd8506834eb9ed622bd213671a3ca
mgubaydullin <mgubaydullin@131.ru>
2025-02-23 17:25:43 +0300

    main 2 fix add isort config
    2 fix add isort config

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,5 @@
 from admin_auto_filters.filters import AutocompleteFilter
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import (Favorite, Ingredient, Recipe, RecipeToIngredient,
